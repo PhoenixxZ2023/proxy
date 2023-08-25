8971d8724f8b646b6cf7413a7f4f890cf366c9d2
PhoenixxZ2023 <101994539+PhoenixxZ2023@users.noreply.github.com>
2023-08-24 22:18:26 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -144,7 +144,7 @@ while true; do
     echo -e "\033[01;31m║\033[0m\033[1;31m[\033[1;36m5\033[1;31m] \033[1;37m• \033[1;33mREINSTALAR PROXY \033[0m"
     echo -e "\033[01;31m║\033[0m\033[1;31m[\033[1;36m6\033[1;31m] \033[1;37m• \033[1;33mSAIR \033[0m"
     echo ""
-    echo -ne "\033[1;32mO QUE DESEJA FAZER\033[1;33m?\033[1;37m"
+    echo -ne "\033[1;32mO QUE DESEJA FAZER?\033[1;33m?\033[1;37m"
     
     read -p "Escolha uma opção: " choice
     
